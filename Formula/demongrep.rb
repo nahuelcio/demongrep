b68e7af5d209ece4062d96e1e605eb3146da6759
Nahuel <cioffinahuel@gmail.com>
2026-03-02 13:08:29 -0300

    Fix SHA256 checksum in Homebrew formula for v1.9.1

--- Formula/demongrep.rb
+++ Formula/demongrep.rb
@@ -2,7 +2,7 @@ class Demongrep < Formula
   desc "Fast, local semantic code search powered by Rust"
   homepage "https://github.com/nahuelcio/demongrep"
   url "https://github.com/nahuelcio/demongrep/archive/refs/tags/v1.9.1.tar.gz"
-  sha256 "1fdf2e8cdacdaeca41004eddbdebaf0fe0cc831e2f086770d81a19935666"
+  sha256 "1fdf2e8cdacdaeca41004edbdebfaf0fe0cc831e2f086770d81a199356661f00"
   license "Apache-2.0"
   head "https://github.com/nahuelcio/demongrep.git", branch: "master"
 
